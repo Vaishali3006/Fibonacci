132ba705e7eb2c6d78f473b0bdbdc5eb6d6a18f6
Vaishali3006 <65015689+Vaishali3006@users.noreply.github.com>
2020-05-09 10:40:30 +0530

    Add files via upload

---
+++ Fibonacci series.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+#fibonacci series
+n3=0
+n1=0
+n2=1
+i=0
+
+while i<20:  
+    """for infinite values write while i>0"""
+    if i<=1:
+        n3=i
+    else:
+        n3=n1+n2
+        n1=n2
+        n2=n3
+    print(n3)
+    i+=1
+print("end of range")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
